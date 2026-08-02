f53c7cc1064bbed975702628102ee6a717701096
Dzedz Timofei <dzedz@internet.ru>
2026-08-02 21:04:49 +0300

    Changed routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -14,11 +16,13 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/chat/{user}', [ChatController::class, 'show'])->name('chat.show');
+
+    Route::get('/messages/{user}', [ChatController::class, 'getMessages'])->name('chat.getmessages');
+    Route::post('/messages/{user}', [ChatController::class, 'sendMessage'])->name('chat.sendMessage');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
